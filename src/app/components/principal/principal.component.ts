8719ce951b1bd452415c67ca75a05a6f96612ae6
rayitodospuntostres <u20202647@utp.edu.pe>
2024-10-18 00:11:37 -0500

    un paso antes del rout

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,12 +1,14 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { HeadComponent } from "./head/head.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [HeadComponent, InicioComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss',
-  
+
 
 })
 export class PrincipalComponent {
