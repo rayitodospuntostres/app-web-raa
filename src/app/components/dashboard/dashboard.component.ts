265457883c1496b2a7e204d46896fbc527d210c7
rayitodospuntostres <u20202647@utp.edu.pe>
2024-10-17 20:20:17 -0500

    errores para lo analice

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
+  encapsulation:ViewEncapsulation.ShadowDom
 })
 export class DashboardComponent {
 
--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
   imports: [],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.scss'
+  styleUrl: './principal.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom
+
 })
 export class PrincipalComponent {
 
